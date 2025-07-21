97d322d0831770fe9046d4284df6632673085ca6
Manal Imran <minalimran2002@gmail.com>
2025-07-22 00:27:02 +0500

    initial commit

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,24 @@
+import arcjet, { tokenBucket, shield, detectBot, slidingWindow } from "@arcjet/node";
+import "dotenv/config";
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+      ],
+    }),
+    
+    // rate limiting
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 30,
+      interval: 5,
+      capacity: 20,
+    }),
+  ],
+});
